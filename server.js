83136e632f474e3d313c199b99f642ac20c3d6e9
Ryan Raposo <raposo.ryan@gmail.com>
2022-07-19 11:41:22 -0400

    use env port

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
 
 function filterByQuery(query, animalsArray) {
