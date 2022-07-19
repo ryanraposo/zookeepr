5fd2b05fbff013be10df672861d805347777842b
Ryan Raposo <raposo.ryan@gmail.com>
2022-07-19 11:50:23 -0400

    add get by id

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -49,6 +51,13 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -56,3 +65,13 @@ app.get('/api/animals', (req, res) => {
     };
     res.json(results);
 })
+
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+});
